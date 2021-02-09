0ce54be62337525a87ae70735424858f2c640f39
randommcsky <46089887+randommcsky@users.noreply.github.com>
2021-02-09 09:43:09 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,13 @@ import "firebase/storage";
 import "firebase/analytics";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCmjQ-QuDiTXe3YBU7zJQl0g5q-_mPt9UY",
-  authDomain: "discord-clone-66d9a.firebaseapp.com",
-  databaseURL: "https://discord-clone-66d9a.firebaseio.com",
-  projectId: "discord-clone-66d9a",
-  storageBucket: "discord-clone-66d9a.appspot.com",
-  messagingSenderId: "265446087212",
-  appId: "1:265446087212:web:392811aa65680282680f89",
-  measurementId: "G-XQPC1S7QLC"
+    apiKey: "AIzaSyB7mgHqb4JFgYdk4vQvS3CTY9C6bNdeH9A",
+    authDomain: "jimhub0.firebaseapp.com",
+    projectId: "jimhub0",
+    storageBucket: "jimhub0.appspot.com",
+    messagingSenderId: "416275417157",
+    appId: "1:416275417157:web:ac2723c9e787ddfd2f7b2c",
+    measurementId: "G-586HRWK1RK"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
