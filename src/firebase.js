f23eb27c45facbd8e5ab15d7e13a3e08de437aad
randommcsky <46089887+randommcsky@users.noreply.github.com>
2021-02-09 09:52:15 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/storage";
 import "firebase/analytics";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyB7mgHqb4JFgYdk4vQvS3CTY9C6bNdeH9A",
-    authDomain: "jimhub0.firebaseapp.com",
-    projectId: "jimhub0",
-    storageBucket: "jimhub0.appspot.com",
-    messagingSenderId: "416275417157",
-    appId: "1:416275417157:web:ac2723c9e787ddfd2f7b2c",
-    measurementId: "G-586HRWK1RK"
+  apiKey: "AIzaSyB7mgHqb4JFgYdk4vQvS3CTY9C6bNdeH9A",
+  authDomain: "jimhub0.firebaseapp.com",
+  projectId: "jimhub0",
+  storageBucket: "jimhub0.appspot.com",
+  messagingSenderId: "416275417157",
+  appId: "1:416275417157:web:ac2723c9e787ddfd2f7b2c",
+  measurementId: "G-586HRWK1RK"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
